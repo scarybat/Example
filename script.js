3d20b91e8983564bb54d4aead4c52c4160b5dac8
scarybat <badmanatpeak@gmail.com>
2024-09-11 15:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
 
     // Display a confirmation message
     const formMessage = document.getElementById('formMessage');
-    formMessage.textContent = `Thank you, ${name}! Your message has been sent.`;
-    formMessage.style.color = 'pink';
+    formMessage.textContent = `WOOO, ${name}! NICE NICE.`;
+    formMessage.style.color = 'red';
 
     // Optionally, you could send the form data to a server here
 });
